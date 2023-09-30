621529f8a3b21abd410eacd0b0de2a7e9c9b7dde
zenodegenkamp <zenodegenkamp@hotmail.com>
2023-09-30 14:48:40 +0200

    changes

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,8 +1,9 @@
-import { OpenAIApi } from 'openai'
+import OpenAI from 'openai';
 
-const apiKey = process.env.OPENAI_API_KEY
-
-const openai = new OpenAIApi(apiKey)
+// New
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+});
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
@@ -22,7 +23,7 @@ const handler = async (event) => {
    
     return {
       statusCode: 200,
-      body: JSON.stringify({ reply: completion.data }),
+      body: JSON.stringify({ reply: completion }),
       
     }
   } catch (error) {
